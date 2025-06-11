ff1aecdd15e84c483944642fee16464c751f8a60
instar94 <sksskdua1234@naver.com>
2025-06-11 21:23:49 +0900

    2025-06-11 SQL 테이블 쿼리 연습

---
+++ 05.SQL/1_table.sql
@@ -0,0 +1,35 @@
+-- 테이블 생성 : 컬럼명 + 데이터 타입
+CREATE TABLE mytable(
+    id INT,
+    name VARCHAR(16)
+);
+
+-- 테이블 조회
+SHOW TABLES;
+
+-- 테이블 정보 조회
+DESC mytable;
+
+-- 테이블 이름 변경
+alter table mytable rename person;
+show tables;
+
+-- 컬럼 추가
+alter table person add column agee double;
+DESC person;
+
+-- 컬럼 데이터 타입 변경
+alter table person modify column agee int;
+DESC person;
+
+-- 컬럼 이름 변경
+alter table person change column agee age int;
+DESC person;
+
+-- 컬럼 삭제
+alter table person drop column age;
+DESC person;
+
+-- 테이블 삭제
+drop table person;
+show tables;
