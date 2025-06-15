bbcf4aeef869069c3ee7f0b5561f0aeba9a38d86
instar94 <sksskdua1234@naver.com>
2025-06-15 11:57:10 +0900

    2025-06-15 다항회귀 및 SQL 파이썬 연동

---
+++ 05.SQL/query/test03.sql
@@ -0,0 +1,4 @@
+create table sql_file (
+    id int primary key,
+    name varchar(100) not null
+);
---
+++ 05.SQL/query/test04.sql
@@ -0,0 +1,4 @@
+INSERT INTO sql_file values (1,"test01.sql");
+INSERT INTO sql_file values (2,"test02.sql");
+INSERT INTO sql_file values (3,"test03.sql");
+INSERT INTO sql_file values (4,"test04.sql");
